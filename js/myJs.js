33bf31ec866d6067c8fa42478a045d1653154889
xintyuio123 <112526501+xintyuio123@users.noreply.github.com>
2022-08-31 08:08:52 +0700

    Add files via upload

--- js/myJs.js
+++ js/myJs.js
@@ -1,17 +1,17 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text1: "hii chào Miu!",
+  text2: "Anh hỏi em cái này nhớ phải trả lời thật lòng nha.",
+  text3: "Em mê anh quá có phải không nào :))",
+  text4: "Nếu không trả lời mà thoát ra là em là một con lợn",
+  text5: "Anh mơ à???",
+  text6: "Mê ơi làm mê <3",
+  text7: "lí do em mơ anh đii :vvvv",
+  text8: "Gửi cho daddi <3",
+  text9: "vừa đẹp trai vừa 17cm ai mà chịu nổi",
+  text10: "Anh biết mà :)), yêu bé bự thế nhờ",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
-  text12: "Okii lunn <3",
+    "tháng 12 này về anh dẫn bé đi chơi nha:v còn giờ thì vào rep tin nhắn anh mau :D",
+  text12: "Okê luon daddi",
 };
 
 $(document).ready(function () {
